41c6c6af38631cdce5abde30de9335c0125f44ed
Jesse Loftus <jesse.loftus@gmail.com>
2018-09-27 11:03:27 -0700

    fixed clicked toggle. added shuffle to images on click

--- src/components/CharlieCard/CharlieCard.js
+++ src/components/CharlieCard/CharlieCard.js
@@ -1,56 +1,89 @@
 import React, { Component } from 'react';
 import './CharlieCard.css';
-import { sampleSize } from 'lodash'
+import { sampleSize } from 'lodash';
 
 class CharlieCard extends Component {
   state = {
-    sample: [],
-    img: [
-'ash.png',
-'bates.png',
-'blade.png',
-'buffalobill.png',
-'carrie.png',
-'chucky.png',
-'cthulhu.png',
-'dracula.png',
-'edward.png',
-'frankenstein.png',
-'freddy.png',
-'furter.png',
-'ghostface.png',
-'hannibal.png',
-'jack.png',
-'jasongritty.png',
-'jason.png',
-'joker.png',
-'kramer.png',
-'lazarou.png',
-'leatherfacegritty.png',
-'leatherface.png',
-'mummy.png',
-'myers.png',
-'nosferatu.png',
-'rick.png',
-'sadako.png',
-'shaun.png',
-'slappy.png',
-'spaulding.png',
-'sweeny.png',
-'wolfman.png',
-'zombie.png',
+    score: 0,
+    topScore: 0,
+    character: [
+      {id: '1',img: 'ash.png', clicked: false},
+      {id: '2',img: 'bates.png', clicked: false},
+      {id: '3',img: 'blade.png', clicked: false},
+      {id: '4',img: 'buffalobill.png', clicked: false},
+      {id: '5',img: 'carrie.png', clicked: false},
+      {id: '6',img: 'chucky.png', clicked: false},
+      {id: '7',img: 'cthulhu.png', clicked: false},
+      {id: '8',img: 'dracula.png', clicked: false},
+      {id: '9',img: 'edward.png', clicked: false},
+      {id:'10' ,img: 'frankenstein.png', clicked: false},
+      {id:'11' ,img: 'freddy.png', clicked: false},
+      {id:'12' ,img: 'furter.png', clicked: false},
+      {id:'13' ,img: 'ghostface.png', clicked: false},
+      {id:'14' ,img: 'hannibal.png', clicked: false},
+      {id:'15' ,img: 'jack.png', clicked: false},
+      {id:'16' ,img: 'jasongritty.png', clicked: false},
+      {id:'17' ,img: 'jason.png', clicked: false},
+      {id:'18' ,img: 'joker.png', clicked: false},
+      {id:'19' ,img: 'kramer.png', clicked: false},
+      {id:'20' ,img: 'lazarou.png', clicked: false},
+      {id:'21' ,img: 'leatherfacegritty.png', clicked: false},
+      {id:'22' ,img: 'leatherface.png', clicked: false},
+      {id:'23' ,img: 'mummy.png', clicked: false},
+      {id:'24' ,img: 'myers.png', clicked: false},
+      {id:'25' ,img: 'nosferatu.png', clicked: false},
+      {id:'26' ,img: 'rick.png', clicked: false},
+      {id:'27' ,img: 'sadako.png', clicked: false},
+      {id:'28' ,img: 'shaun.png', clicked: false},
+      {id:'29' ,img: 'slappy.png', clicked: false},
+      {id:'30' ,img: 'spaulding.png', clicked: false},
+      {id:'31' ,img: 'sweeny.png', clicked: false},
+      {id:'32' ,img: 'wolfman.png', clicked: false},
+      {id:'33' ,img: 'zombie.png', clicked: false},
     ]
   };
+
+  gameLose = () => {
+
+  };
+
+  handleClick = id => {
+    let clickState = this.state.character.find(char => char.id === id);
+console.log(clickState)
+    if(clickState.clicked) {
+      this.gameLose();
+    } else {
+      clickState.clicked = true;
+      let newChars = this.state.character.filter(char => char.id !== id);
+      newChars.push(clickState)
+      console.log(`New clicked state ${clickState.clicked} for ${clickState.img}`)
+
+      this.setState({
+        ...this.state,
+        score: this.state.score +1,
+        topScore: this.state.score === this.state.topScore
+          ? this.state.topScore + 1
+          : this.state.topScore,
+        character: newChars
+      })
+    }
+  };
+
+  sample = sampleSize(this.state.character, 10);
 
   render() {
     return (
       <div>
-        {sampleSize(this.state.img, 3).map(image => (
+        { this.sample.map(image => (
           <img 
-          key={image}
+          key={image.id}
+          id={image.id}
+          alt={`${image.img}`}
           className='CharlieCard'
-          src={`/img/charlie-brown/${image}`} />)
-        )}
+
+          onClick={ () => this.handleClick(image.id) }
+          src={`/img/charlie-brown/${image.img}`} />)
+        ).sort( () => Math.random() - 0.5) }
       </div>
     )
   };
